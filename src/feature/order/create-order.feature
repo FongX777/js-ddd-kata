bbe6f9dc4d8210976e688fa0c79026eba2d4e9ec
fong <fong@meepshop.com>
2020-08-18 11:15:53 +0800

    Fix cucumber

--- src/feature/order/create-order.feature
+++ src/feature/order/create-order.feature
@@ -5,47 +5,47 @@ Feature: Shopper Places an order
     so that I can buy things I want.
 
     Constraints:
-    * Order has a status
+    * Order has an init status 'PROCESSING'
     * An order should have a recipient's name, email, phone number & shipping address
     * an order can only has at max 8 unique items
-    * Each item can only has at max 5 pieces
+    * Maximum qauntity of each item is 5
     * Max order amount should not exceed 10,000
+
+  Background:
+    Given the recipient information:
+      | name | email         | phoneNumber | shippingAddress          |
+      | Sam  | sam@gmail.com | 0912121212  | Taipei Xinyi Road No 10. |
 
 
   Scenario: Once the cart is checked out, an order is created
-    Given a shopper has had one product A and one product B in the cart
-    And the total order amount is 1000
-    And the shopper has input recipient information
-      | name | email         | phoneNumber | shippingAddress          |
-      | Sam  | sam@gmail.com | 0912121212  | Taipei Xinyi Road No 10. |
+    Given a shopper has 2 products with total amount 1000 in the cart
+    And the shopper has input enough recipient information
     When the cart is checked out
     Then the order is created
     And a paymentLink is created
     And the status should be "PROCESSING"
 
   Scenario: Max 8 unique items in one order
-    Given a shopper has 9 products in the cart
-    And the total order amount is 1000
+    Given a shopper has 9 products with total amount 1000 in the cart
     And the shopper has input enough recipient information
     When the cart is checked out
     Then the order should not be created
     And the error message should be "[Create Order Error] Max 8 items"
 
 
-  Scenario: Each item can only has at max 5 pieces
-    Given a shopper has 6 a products in the cart
-    And the total order amount is 1000
+  Scenario: Each item can only has at max 5 quantity
+    Given a shopper has 1 product with 6 quantity in the cart
     And the shopper has input enough recipient information
     When the cart is checked out
     Then the order should not be created
-    And the error message should be "Exceed Max Items Purchase Limit Allowed"
+    And the error message should be "[Create Order Error] Item 'Test Product Name' has invalid quantity '6'"
 
-  Scenario: Max order amount should not exceed 10,000
-    Given a shopper has 6 a products with total 10,001 in the cart
+  Scenario: Max order amount should not exceed 10,0000
+    Given a shopper has 6 products with total amount 100001 in the cart
     And the shopper has input enough recipient information
     When the cart is checked out
     Then the order should not be created
-    And the error message should be "Exceed Max Order Amount Allowed"
+    And the error message should be "[Create Order Error] Order amount should not surpass 10,0000"
 
 
 
--- src/feature/step_definitions/create_order.feature.js
+++ src/feature/step_definitions/create_order.feature.js
@@ -1,22 +1,15 @@
-const { Order } = require('../../domain/model/order');
 const { CreateOrderUseCase } = require('../../use_case/order/create_order');
+const { v4: uuidv4 } = require('uuid');
 const {
   InMemOrderRepository,
 } = require('../../use_case/__test__/repository/order_repo');
 
-const getSampleRecipientParam = () =>
-  CreateOrderUseCase.toRecipient({
-    name: 'Bella Recipient',
-    email: 'bella@meepshop.com',
-    phoneNumber: '0987121212',
-    shippingAddress: 'Taipei District Xinyi Renai Road No 188',
-  });
-const getSampleItemParam = () =>
+const getSampleItemParam = (unitPrice, q) =>
   CreateOrderUseCase.toItem({
-    productId: '32e5d058-398d-483f-a7d1-92e5af4e81db',
+    productId: uuidv4(),
     name: 'Test Product Name',
-    quantity: 3,
-    unitPrice: 10,
+    quantity: q,
+    unitPrice,
   });
 
 const assert = require('assert');
@@ -27,24 +20,37 @@ Before(function () {
   const usecase = new CreateOrderUseCase(orderRepo);
   this.orderRepo = orderRepo;
   this.usecase = usecase;
+  this.input = {};
+});
+
+Given('the recipient information:', function (dataTable) {
+  const recipientData = dataTable.hashes()[0];
+  this.recipient = CreateOrderUseCase.toRecipient({
+    name: recipientData.name,
+    email: recipientData.email,
+    phoneNumber: recipientData.phoneNumber,
+    shippingAddress: recipientData.shippingAddress,
+  });
 });
 
 Given(
-  'a shopper has had one product A and one product B in the cart',
-  function () {
-    this.input = {};
-    this.input.items = [getSampleItemParam(), getSampleItemParam()];
+  'a shopper has {int} products with total amount {int} in the cart',
+  function (numOfItems, totalAmount) {
+    const price = totalAmount / numOfItems;
+    this.input.items = new Array(numOfItems).fill(getSampleItemParam(price, 1));
   }
 );
+Given('a shopper has 1 product with {int} quantity in the cart', function (
+  quantity
+) {
+  this.input.items = new Array(1).fill(getSampleItemParam(100, quantity));
+});
 
-Given('the total order amount is {float}', function (float) {});
-
-Given('the shopper has input recipient information', function (dataTable) {
-  this.input.recipient = getSampleRecipientParam();
+Given('the shopper has input enough recipient information', function () {
+  this.input.recipient = this.recipient;
 });
 
 When('the cart is checked out', async function () {
-  // Write code here that turns the phrase above into concrete actions
   const result = await this.usecase.execute({
     items: this.input.items,
     recipient: this.input.recipient,
@@ -53,36 +59,17 @@ When('the cart is checked out', async function () {
 });
 
 Then('the order is created', function () {
-  // Write code here that turns the phrase above into concrete actions
-  assert.equal(this.result.error, undefined);
-  const order = this.orderRepo.getFirstOne();
-  assert.notEqual(order, undefined);
+  const order = this.result.order;
+  assert.equal(order.id.length, 36);
 });
 
 Then('a paymentLink is created', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return '';
 });
 
-Then('the status should be {string}', function (string) {
-  const order = this.orderRepo.getFirstOne();
-  assert.equal(this.result.order.status, string);
-  assert.equal(order.status, string);
-});
-
-Given('a shopper has {int} products in the cart', function (numOfItems) {
-  this.input = {};
-  this.input.items = new Array(numOfItems).fill(getSampleItemParam());
-});
-
-// Given('the total order amount is {int}', function (int) {
-// // Given('the total order amount is {float}', function (float) {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-Given('the shopper has input enough recipient information', function () {
-  this.input.recipient = getSampleRecipientParam();
+Then('the status should be {string}', function (expectedStatus) {
+  const order = this.result.order;
+  assert.equal(order.status, expectedStatus);
 });
 
 Then('the order should not be created', function () {
@@ -90,71 +77,7 @@ Then('the order should not be created', function () {
   assert.equal(order, undefined);
 });
 
-Then('the error message should be {string}', function (msg) {
+Then('the error message should be {string}', function (expectedErrMsg) {
   const error = this.result.error;
-  assert.equal(error.message, msg);
+  assert.equal(error.message, expectedErrMsg);
 });
-
-// Given('a shopper has {int} a products in the cart', function (int) {
-// // Given('a shopper has {float} a products in the cart', function (float) {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// Given('the total order amount is {int}', function (int) {
-// // Given('the total order amount is {float}', function (float) {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// Given('the shopper has input enough recipient information', function () {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// When('the cart is checked out', function () {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// Then('the order should not be created', function () {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// Then('the error message should be {string}', function (string) {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// Given('a shopper has {int} a products with total {int},{int} in the cart', function (int, int2, int3) {
-// // Given('a shopper has {int} a products with total {int},{float} in the cart', function (int, int2, float) {
-// // Given('a shopper has {int} a products with total {float},{int} in the cart', function (int, float, int2) {
-// // Given('a shopper has {int} a products with total {float},{float} in the cart', function (int, float, float2) {
-// // Given('a shopper has {float} a products with total {int},{int} in the cart', function (float, int, int2) {
-// // Given('a shopper has {float} a products with total {int},{float} in the cart', function (float, int, float2) {
-// // Given('a shopper has {float} a products with total {float},{int} in the cart', function (float, float2, int) {
-// // Given('a shopper has {float} a products with total {float},{float} in the cart', function (float, float2, float3) {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// Given('the shopper has input enough recipient information', function () {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// When('the cart is checked out', function () {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// Then('the order should not be created', function () {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
-
-// Then('the error message should be {string}', function (string) {
-//   // Write code here that turns the phrase above into concrete actions
-//   return 'pending';
-// });
